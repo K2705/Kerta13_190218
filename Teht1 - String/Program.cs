1d77e52027cd7868d949a685ba7b340e41f84815
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-02-19 15:22:34 +0200

    T1 : strings

--- Teht1 - String/Program.cs
+++ Teht1 - String/Program.cs
@@ -8,8 +8,32 @@ namespace Teht1___String
 {
     class Program
     {
+        delegate void ModifyString(string str);
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Enter text\n> ");
+                string input = Console.ReadLine();
+                Console.WriteLine("What would you like to do to the text?");
+                Console.WriteLine("[1] Uppercase");
+                Console.WriteLine("[2] Lowercase");
+                Console.WriteLine("[3] Title case");
+                Console.WriteLine("[4] Reverse");
+                Console.WriteLine("[Q] Quit");
+                ModifyString edit = delegate(string str) { };
+                string action = Console.ReadLine();
+
+                if (action.Contains("1")) { edit += StringEdit.PrintUpper; }
+                if (action.Contains("2")) { edit += StringEdit.PrintLower; }
+                if (action.Contains("3")) { edit += StringEdit.PrintTitleCase; }
+                if (action.Contains("4")) { edit += StringEdit.PrintReverse; }
+
+                edit(input);
+
+                if (action.Contains("Q") || action.Contains("q")) { break; }
+            }
         }
     }
 }
---
+++ Teht1 - String/StringEdit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht1___String
+{
+    static class StringEdit
+    {
+        public static void PrintUpper(string str)
+        {
+            Console.WriteLine(str.ToUpper());
+        }
+
+        public static void PrintLower(string str)
+        {
+            Console.WriteLine(str.ToLower());
+        }
+
+        public static void PrintTitleCase(string str)
+        {
+            if (str.Length > 0)
+            {   //TODO: Stop changing characters to strings and back it's dumb
+                char[] print = new char[str.Length];
+                print[0] = str[0].ToString().ToUpper()[0];
+                for (int i = 1; i < str.Length; i++)
+                {
+                    print[i] = str[i].ToString().ToLower()[0];
+                }
+                Console.WriteLine(new string(print));
+            }
+        }
+
+        public static void PrintReverse(string str)
+        {
+            char[] print = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                print[i] = str[str.Length - 1 - i];
+            }
+            Console.WriteLine(new string(print));
+        }
+    }
+}
